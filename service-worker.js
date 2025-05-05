52b6acb23e14ebb41cc3ec2eb2424416ddf7a22e
abcde439 <malamembun135@gmail.com>
2025-05-05 17:54:02 +0700

    try 2 add PWA

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("pwa-cache").then((cache) => {
+            return cache.addAll(["/", "/index.html", "/styles.min.css", "/script.min.js", "/icons/icon-192.png", "/icons/icon-512.png"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
